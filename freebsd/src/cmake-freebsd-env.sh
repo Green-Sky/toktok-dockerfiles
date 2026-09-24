55ffab95b75b8025a7387ce533adb9fb195221d1
Green Sky <Green-Sky@users.noreply.github.com>
2026-09-24 10:38:40 +0200

    chore(freebsd): update freebsd image to 14.5
    Fixes trying to fetch old image from new only remote path.

--- freebsd/src/cmake-freebsd-env.sh
+++ freebsd/src/cmake-freebsd-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright (C) 2018-2023 nurupo
+# Copyright © 2018-2023 nurupo
+# Copyright © 2026 The TokTok team
 
 # Common variables and functions
 
@@ -8,7 +9,7 @@ NPROC="$(nproc)"
 SCREEN_SESSION=freebsd
 SSH_PORT=10022
 
-FREEBSD_VERSION="14.3"
+FREEBSD_VERSION="14.5"
 IMAGE_NAME="FreeBSD-$FREEBSD_VERSION-RELEASE-amd64.qcow2"
-# https://download.freebsd.org/ftp/releases/VM-IMAGES/14.3-RELEASE/amd64/Latest/
-IMAGE_SHA512="9024ea7060181855ee5582d52d015e03672a046ce755000c30b2298813dc956effbaf505d750412cf04697c8970d6b11b1db12e18ad0df58d9f691ce20dddea0"
+# https://download.freebsd.org/ftp/releases/VM-IMAGES/14.5-RELEASE/amd64/Latest/
+IMAGE_SHA512="9960eea5f5dfbd7e95de023dbf3cc5771dabe1dd633a98f2069ac99b3de8d444455df313fc15da5de8d149cfb5d65eee25526d7fb30bb04f75b8c65a85e59bf6"
